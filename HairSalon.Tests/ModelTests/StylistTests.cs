28b8797f43c4117f10af9ee380642ee1501e3002
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-12-07 16:49:02 -0800

    Stylist constructor returns the same type test passes

--- HairSalon.Tests/ModelTests/StylistTests.cs
+++ HairSalon.Tests/ModelTests/StylistTests.cs
@@ -1,27 +1,28 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using HairSalon.Models;
-// using System.Collections.Generic;
-// using System;
-//
-// namespace HairSalon.Tests
-// {
-//   [TestClass]
-//   public class StylistTest : IDisposable
-//   {
-//     public void Dispose()
-//     {
-//       Stylist.ClearAll();
-//       // Client.ClearAll();
-//     }
-//     public StylistTest()
-//     {
-//       DBConfiguration.ConnectionString = "server=localhost;userid=root;password=root;port=8889;database=chris_rudnicky_test;";
-//     }
-//
-//     [TestMethod]
-//     public void StylistConstructor_CreatesInstanceOfStylist_Stylist()
-//     {
-//
-//     }
-//   }
-// }
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System;
+
+namespace HairSalon.Tests
+{
+  [TestClass]
+  public class StylistTest : IDisposable
+  {
+    public void Dispose()
+    {
+      // Stylist.ClearAll();
+      // Client.ClearAll();
+    }
+    public StylistTest()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;userid=root;password=root;port=8889;database=chris_rudnicky_test;";
+    }
+
+    [TestMethod]
+    public void StylistConstructor_CreatesInstanceOfStylist_Stylist()
+    {
+      Stylist newStylist = new Stylist ("hannah");
+      Assert.AreEqual(typeof(Stylist), newStylist.GetType());
+    }
+  }
+}
