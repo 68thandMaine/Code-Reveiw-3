a07423817246689defb4e0857f3cea185391ad22
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-12-09 00:17:25 -0800

    Add New returns correct view test for stylist controller

--- HairSalon.Tests/ControllerTests/StylistController.cs
+++ HairSalon.Tests/ControllerTests/StylistController.cs
@@ -15,6 +15,22 @@ namespace HairSalon.Tests
       StylistController controller = new StylistController();
       ActionResult indexView = controller.Index();
       Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+    }
+    // Could not get to fail. 
+    // [TestMethod]
+    // public void Index_HasCorrectModelType_StylistList()
+    // {
+    //   ViewResult indexView = new HomeController().Index() as ViewResult;
+    //   var result = indexView.ViewData.Model;
+    //   Assert.IsInstanceOfType(result, typeof());
+    // }
+    [TestMethod]
+    public void New_ReturnsCorrectView_True()
+    {
+      StylistController controller = new StylistController();
+      ActionResult newView = controller.New();
+      Assert.IsInstanceOfType(newView, typeof(ViewResult));
+    }
+
   }
-}
 }
--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -12,10 +12,18 @@ namespace HairSalon.Controllers
       List<Stylist> allStylists = Stylist.GetAll();
       return View(allStylists);
     }
-    // [HttpGet("/stylist/new")]
-    // public ActionResult New()
-    // {
-    //
-    // }
+    [HttpGet("/stylist/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/stylist")]
+    public ActionResult Create(string stylistName)
+    {
+      Stylist newStylist = new Stylist (stylistName);
+      newStylist.Save();
+      List<Stylist> allStylists = Stylist.GetAll();
+      return View("Index", allStylists);
+    }
   }
 }
