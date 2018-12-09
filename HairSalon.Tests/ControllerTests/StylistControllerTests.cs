b04339292ebc77857643da7fcbc897e4ceb96cbd
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-12-09 10:36:30 -0800

    add new test for show containing correct model type

--- HairSalon.Tests/ControllerTests/StylistControllerTests.cs
+++ HairSalon.Tests/ControllerTests/StylistControllerTests.cs
@@ -7,7 +7,7 @@ using HairSalon.Models;
 namespace HairSalon.Tests
 {
   [TestClass]
-  public class StylistControllerTest
+  public class StylistControllerTests
   {
     [TestMethod]
     public void Index_ReturnsCorrectView_True()
@@ -16,7 +16,7 @@ namespace HairSalon.Tests
       ActionResult indexView = controller.Index();
       Assert.IsInstanceOfType(indexView, typeof(ViewResult));
     }
-    // Could not get to fail. 
+    // Could not get to fail.
     // [TestMethod]
     // public void Index_HasCorrectModelType_StylistList()
     // {
@@ -31,6 +31,21 @@ namespace HairSalon.Tests
       ActionResult newView = controller.New();
       Assert.IsInstanceOfType(newView, typeof(ViewResult));
     }
+    [TestMethod]
+    public void Show_ReturnsCorrectView_True()
+    {
+      StylistController controller = new StylistController();
+      ActionResult newView = controller.Show(1);
+      Assert.IsInstanceOfType(newView, typeof(ViewResult));
+    }
+    [TestMethod]
+    public void Show_HasCorrectModelType_ListOfClients()
+    {
+      ViewResult showView = new StylistController().Show(1) as ViewResult;
 
+      var result = showView.ViewData.Model;
+
+      Assert.IsInstanceOfType(result, typeof(List<Client>));
+    }
   }
 }
--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace HairSalon.Controllers
 {
-  public class StylistsController : Controller
+  public class StylistController : Controller
   {
     [HttpGet("/stylists")]
     public ActionResult Index()
@@ -18,13 +18,23 @@ namespace HairSalon.Controllers
     {
       return View();
     }
-    [HttpPost("/stylist")]
+    [HttpPost("/stylists")]
     public ActionResult Create(string stylistName)
     {
       Stylist newStylist = new Stylist (stylistName);
       newStylist.Save();
       List<Stylist> allStylists = Stylist.GetAll();
       return View("Index", allStylists);
+    }
+    [HttpGet("/stylists/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Stylist selectedStylist = Stylist.Find(id);
+      List<Client> stylistClients = selectedStylist.GetClients();
+      model.Add("stylist", selectedStylist);
+      model.Add("clients", stylistClients);
+      return View();
     }
   }
 }
