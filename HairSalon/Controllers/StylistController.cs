353ee173ff444451645492a5eb4004b995a093e7
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-12-08 23:32:14 -0800

    Add failing and passing test for Stylist controller index view

--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace HairSalon.Controllers
 {
-  public class StylistController : Controllers
+  public class StylistController : Controller
   {
     [HttpGet("/stylists")]
     public ActionResult Index()
@@ -12,10 +12,10 @@ namespace HairSalon.Controllers
       List<Stylist> allStylists = Stylist.GetAll();
       return View(allStylists);
     }
-    [HttpGet("/stylist/new")]
-    public ActionResult New()
-    {
-      
-    }
+    // [HttpGet("/stylist/new")]
+    // public ActionResult New()
+    // {
+    //
+    // }
   }
 }
